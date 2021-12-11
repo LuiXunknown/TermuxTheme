ce92d887d41463886d72f4a08ac8c130b66f6f8d
kira <93389526+L0rdK1r422@users.noreply.github.com>
2021-12-11 15:33:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 clear
 cp /data/data/com.termux/files/home/CustomizeTermux/bash.bashrc /data/data/com.termux/files/usr/etc/
 sleep 5s
+echo 
+echo 
+echo
+berhasil:)
+echo
+sleep 3s
+echo login..
+
+
 login
 
