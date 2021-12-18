e2c0017e58567ae6487d02515fb67952ba74e434
kira <93389526+L0rdK1r422@users.noreply.github.com>
2021-12-18 16:43:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 clear
 rm -rf /data/data/com.termux/files/usr/etc/motd-playstore
 rm -rf /data/data/com.termux/files/usr/etc/motd.dpkg-old
+rm -rf /data/data/com.termux/files/usr/etc/motd
 cp /data/data/com.termux/files/home/TermuxTheme/bash.bashrc /data/data/com.termux/files/usr/etc/
 sleep 5s
 echo 
